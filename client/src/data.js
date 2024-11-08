79d63455d62ca4ee7d71ff58aa815ee3e8ac71dd
basu-saxena <basusaxena2003@gmail.com>
2024-11-08 20:36:48 +0530

    slider add

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "/Media/Products/Mens-Standard-Fit-Crew-T-Shirt01-600x764-removebg-preview.png",
+    title: "SUMMER SALE",
+    decs: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFFFOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "/Media/Products/Mens-Standard-Fit-Sherpa-Fleece-Jacket01-600x764-removebg-preview.png",
+    title: "WINTER SALE",
+    decs: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFFFOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "/Media/Products/Mens-Standard-Fit-Sweater-Fleece-Jacket01-600x764-removebg-preview.png",
+    title: "AUTUMN SALE",
+    decs: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFFFOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
