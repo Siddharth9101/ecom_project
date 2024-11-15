8c1b14609450db7138eb6e6aa67982d5b4ac8ca0
basu-saxena <basusaxena2003@gmail.com>
2024-11-15 12:47:46 +0530

    UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Product></Product>;
+  return <Cart></Cart>;
 };
 
 export default App;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("/Media/pexels-bohlemedia-1884584.jpg") center;
+  background-size: contain;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  background-color: white;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  user-select: none;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+  user-select: none;
+`;
+
+const Button = styled.button`
+  border: none;
+  width: 40%;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an acoount, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
